e2606407ffa022946f6688b06657b0e447459106
deepika singh <deepika3715@gmail.com>
2023-01-25 17:01:21 +0530

    Adding Weather & NavBar Component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import WeatherPane from './WeatherPane';
+//import WeatherState from './context/weather/WeatherState';
+import NavigationBar from './NavigationBar';
+
 
 function App() {
   return (
     <div className="App">
+      <NavigationBar/>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <WeatherPane/>  
       </header>
     </div>
   );
---
+++ src/NavigationBar.js
@@ -0,0 +1,45 @@
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import myLoc from './images/icons8-place-marker-16.png';
+import AppLogo2 from './images/AppLogo2.png';
+
+function NavigationBar(props) {
+  return (
+<nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <a className="navbar-brand" href="#">WeatherApp</a>
+
+  <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+    <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li className="nav-item active">
+        <a className="nav-link" href="#">Today <span className="sr-only">(current)</span></a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">3 Day</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">7 Day</a>
+      </li>
+    </ul>
+    <form className="form-inline my-2 my-lg-0">
+    <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li className="nav-item">
+        <p className="nav-link" href="#"><img src={myLoc} alt="my-gif" /> {props.region} </p>
+      </li>
+      </ul>
+      <input className="form-control mr-sm-2" type="search" placeholder="Search for a city" aria-label="Search" />
+      <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search </button>
+    </form>
+  </div>
+</nav>
+
+  );
+}
+
+export default NavigationBar;
